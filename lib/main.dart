d307a1dd7fd44292e052556855d8f95a23bad466
rangerx-dark <ajaykumar999jaiswal@gmail.com>
2023-01-16 01:46:56 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/Homepage.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/Login_page.dart';
 
 void main() {
@@ -19,7 +19,10 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     String day = "Ajay";
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.light),
       initialRoute: "/login",
       routes: {"/home": (context) => Home(), "/login": (context) => Login()},
